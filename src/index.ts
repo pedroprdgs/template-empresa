74a63972c9d3ca2f8f1603bdac540b477765e6c9
Pedro <pedropereirarodrigues16@gmail.com>
2026-09-10 17:05:38 -0300

    feat: criado módulo de empresa

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Cliente } from "./Cliente";
+export { default as Empresa } from "./Empresa";
+export { default as Endereco } from "./Endereco";
+export { default as Telefone } from "./Telefone";
